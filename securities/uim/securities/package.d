3440ef4591cde98043ca76d860b211851fc804a7
UIMSolutions <ui.manufaktur@gmail.com>
2025-03-04 18:28:33 +0100

    Add package structure and initial implementations for uim.securities modules

---
+++ securities/uim/securities/package.d
@@ -0,0 +1,32 @@
+/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.securities;
+
+mixin(ImportPhobos!());
+
+// Dub
+public {
+  import colored;
+  import vibe.d;
+  import vibe.http.session : HttpSession = Session;
+}
+
+public { // uim libraries
+  import uim.core;
+  import uim.oop;
+  import uim.errors;
+}
+
+public { // uim.filesystem libraries
+  import uim.securities.classes;
+  import uim.securities.enumerations;
+  import uim.securities.errors;
+  import uim.securities.exceptions;
+  import uim.securities.helpers;
+  import uim.securities.interfaces;
+  import uim.securities.mixins;
+  import uim.securities.tests;
+}
